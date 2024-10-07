d6a8d942684e1bf6bb67c831f59b64d7d1c34e6b
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2024-10-06 22:29:08 -0500

    Segundo commit

--- Ingresar_usuario.py
+++ Ingresar_usuario.py
@@ -1,28 +1,30 @@
 def info(usuarios):
+    usuario = {
+            'nombre': [],
+            'nickname': []
+            }
+    usuarios.append(usuario)
     while True:
         print("""
         -----------------------------
             REGISTRO DE USUARIO     
         -----------------------------
         """)
-        usuario = {
-            'nombre': [],
-            'nickname': []
-            }
         
-        usuarios.append(usuario)
-        nickname_registrado = {}
         
         nombre_delusuario = input("Ingresa tu nombre completo -> ")
         nickname_delusuario = input ("Ingresa tu nickname -> ")
 
-        if nickname_delusuario in nickname_registrado:
+        if nickname_delusuario in usuario["nickname"]:
             print("Este nickname YA ESTA REGISTRADO, Ingresa uno diferente")
-            w= input("Preciona ENTER para continuar -> ")
+            w= input("Preciona ENTER para volver a intentar -> ") 
             continue
-        usuario['nombre'].append(nombre_delusuario)
-        usuario['nickname'].append(nickname_delusuario)
-        ingresar_otro =input("Te gustaria ingresar otro Usuario? SI(1) NO(2) -> ")
-        if ingresar_otro != '1':
-            w=input("Presiona ENTER para continuar -> ")
-            break
+        else:
+            usuario['nombre'].append(nombre_delusuario)
+            usuario['nickname'].append(nickname_delusuario)
+
+            ingresar_otro =input("Te gustaria ingresar otro Usuario? SI(1) NO(2) -> ")
+            if ingresar_otro != '1':
+                w=input("Presiona ENTER para continuar -> ")
+                break
+    print(usuario)
